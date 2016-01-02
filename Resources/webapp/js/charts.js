c877090c1539a12121613c91fa8a53b4d27ecde3
jacob5555555555 <jacobeliasprinz@gmail.com>
2016-01-01 19:10:51 -0500

    Add ability to download chart as .csv file

--- Resources/webapp/js/charts.js
+++ Resources/webapp/js/charts.js
@@ -21,31 +21,57 @@ function httpPostAsync(theUrl, data, callback)
     xmlHttp.send(data);
 }
 
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+function dataToCsv(dataObj){
+    var text = dataObj.xAxisLabel + "," + dataObj.yAxisLabel;
+    for (var point of dataObj.dataPoints){
+        text += "\n" + point.x + "," + point.y;
+    }
+    return text;
+}
+
 function makeChart(name, dataObj){
-    console.log(name);
-    console.log(dataObj);
     var div = document.createElement("div");
     var nameP = document.createElement("p");
     var chart = document.createElement("canvas");
+    var downloadButton = document.createElement("button");
     
     var ctx = chart.getContext("2d");
     
     nameP.innerHTML = name;
     
     var data = [
-    {
-      label: 'data',
-      strokeColor: '#007ACC',
-      pointColor: '#007ACC',
-      pointStrokeColor: '#fff',
-      data: dataObj.dataPoints
-    }
-  ];
+        {
+          label: 'data',
+          strokeColor: '#007ACC',
+          pointColor: '#007ACC',
+          pointStrokeColor: '#fff',
+          data: dataObj.dataPoints
+        }
+    ];
+    
+    downloadButton.addEventListener("click", function(){
+       download(name + ".csv", dataToCsv(dataObj));
+    });
+    downloadButton.innerText = "Download Chart";
     
     var myNewChart = new Chart(ctx).Scatter(data);
     
     div.appendChild(nameP);
     div.appendChild(chart);
+    div.appendChild(downloadButton);
     return div;
 }
 
